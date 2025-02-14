8ca5b9f3426493cc1f2567279feb2779dd1167e2
naveenkumar-interns <naveenkumar.intern@gmail.com>
2025-02-14 21:00:17 +0530

    yake

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import pymongo
 # from langchain_mistralai import MistralAIEmbeddings
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from typing import List
+import yake
 
 client = pymongo.MongoClient("mongodb+srv://jsckson_store:jsckson_store@cluster0.9a981.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["jacksonHardwareDB"]
@@ -38,41 +39,81 @@ keywords = ""
 memory = ConversationBufferWindowMemory(return_messages=True, k=2)
 
 
+
+embedding_cache = {}
+
+
 def generate_embedding(text: str) -> List[float]:
+    if text in embedding_cache:
+        return embedding_cache[text]
 #   embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     # embeddings = NomicEmbeddings(model="nomic-embed-text-v1.5", nomic_api_key=os.getenv("NOMIC_AI_APIKEY"))
     # embeddings = MistralAIEmbeddings(model="mistral-embed", api_key=os.getenv("MISTRAL_AI_APIKEY"))
     embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=os.getenv("hf_token"), model_name="sentence-transformers/all-MiniLM-l6-v2")
     response = embeddings.embed_query(text)
+    embedding_cache[text] = response
     return response
 
 
-def get_keywords(input_text):
-    try:
+# def get_keywords(input_text):
+#     try:
 
-        prompt = ChatPromptTemplate.from_messages([
-    (
-        "system",
-        """ you are system that helps to extract main keywords from the user query.
-        output should be a list of keywords that are extracted from the user query without brackets.
-        """,
-    ),
-    (
-        "human",
-        "{input}"
-    ),
-])
-        chain = prompt | llm
-        response = chain.invoke({"input": input_text})
-        global keywords
-        keywords +=response.content
-        if len(keywords) > 50:
-            keywords = keywords[10:]
-        return response.content
+#         prompt = ChatPromptTemplate.from_messages([
+#     (
+#         "system",
+#         """ you are system that helps to extract main keywords from the user query.
+#         output should be a list of keywords that are extracted from the user query without brackets.
+#         """,
+#     ),
+#     (
+#         "human",
+#         "{input}"
+#     ),
+# ])
+#         chain = prompt | llm
+#         response = chain.invoke({"input": input_text})
+#         global keywords
+#         keywords +=response.content
+#         if len(keywords) > 50:
+#             keywords = keywords[10:]
+#         return response.content
         
+#     except Exception as e:
+#         print(f"Error in get_response: {str(e)}")
+#         raise
+
+
+def get_keywords(user_query):
+    try:
+        # Initialize YAKE keyword extractor
+        language = "en"
+        max_ngram_size = 3
+        deduplication_threshold = 0.9
+        numOfKeywords = 4
+        yake_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+
+        # Extract keywords using YAKE
+        extracted_keywords = yake_extractor.extract_keywords(user_query)
+
+        # Extract the keywords from the result
+        k = [k for k, score in extracted_keywords]
+
+        # Remove duplicates and return the list of keywords
+        result = list(set(k))
+        global keywords
+        keywords = " ".join(result)
+        keywords += " ".join(result)
+        if len(keywords) > 80:
+            keywords = keywords[10:]
+
+        print(keywords)
+
+        return result
+    
     except Exception as e:
-        print(f"Error in get_response: {str(e)}")
+        print(f"Error in extract_keywords_yake: {str(e)}")
         raise
+
 
 def convert_to_json(data):
     result = []
